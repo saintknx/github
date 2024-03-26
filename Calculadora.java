12acf11a9dc677d464fbec6ab79980c415fdeea4
Kaua Sousa <kauasantos2014s10@gmail.com>
2024-03-26 19:06:12 -0300

    apostila(git-java): funcao dividir implementada

--- Calculadora.java
+++ Calculadora.java
@@ -8,4 +8,7 @@ public class Calculadora{
     public double multiplicar(double a, double b){
         return a * b;
     }
+    public double dividir(double a, double b){
+        return a / b;
+    }
 }
